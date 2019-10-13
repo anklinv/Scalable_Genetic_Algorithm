76f8e272588d1e8365326243089aeace0a287832
MacPherson Justin <justin.macpherson@scs.ch>
2019-10-13 17:51:42 +0200

    bug fixes

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,8 @@
+"""
+    use as python preprocess.py <file-name>
+"""
+
+
 import numpy as np
 import sys
 
@@ -10,14 +15,13 @@ cities_list = []
 with open(tsp_file) as f:
     lns = f.readlines()
     dim = int(lns[3].split(' ')[2])
-    print(dim)
     node_edge_incidence = np.empty((dim,dim))
     location = np.empty((dim,2))
     for i in range(0,len(lns)-7):
         location[i,:] = lns[i+6].split(' ')[1:]
     for i in range(0,dim-1):
-        for j in range(1,dim):
-            node_edge_incidence[i,j] = np.sqrt((location[i,0] - location[j,0])**2 + (location[i,1] - location[j,1])**2)
+        for j in range(0,dim):
+            node_edge_incidence[i,j] = int(np.sqrt((location[i,0] - location[j,0])**2 + (location[i,1] - location[j,1])**2))
         
-    np.savetxt(tsp_name +'.csv', node_edge_incidence)
+    np.savetxt(tsp_name +'.csv', node_edge_incidence, fmt='%d', delimiter=',',newline='\n')
         
