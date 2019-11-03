b81d92c249aaced24e95251e5de419e4d9a72434
Sebastian Kurella <sebastian.kurella@celerity.audio>
2019-11-03 02:01:43 +0100

    Implement key-value logging with wall and CPU clocks

--- logging/logging.cpp
+++ logging/logging.cpp
@@ -1,7 +1,9 @@
+#include <ctime>
 #include <sstream>
 #include <iomanip>
 
 #include "logging.hpp"
+#include "tags.hpp"
 
 #define LOGGING_DIRECTORY "logs/"
 
@@ -22,14 +24,43 @@ Logger::Logger(int rank, time_t *time) {
 void Logger::open() {
     this->fitness_file.open(this->filename_prefix + "_fitness.csv");
     // this->fitness_file << "epoch, fitness" << std::endl;
+    this->tag_file.open(this->filename_prefix + "_tags.bin", std::ios::out | std::ios::binary);
+    log_tag_value(LOGGING_TAG_VERSION, LOGGING_VERSION);
+    log_tag_value(LOGGING_TAG_CLOCKS_PER_SEC, CLOCKS_PER_SEC);
+
+    this->clock = std::chrono::high_resolution_clock();
+    this->start = this->clock.now();
+    log_wall_clock(LOGGING_TAG_WC_LOGGING_BEGIN);
+    log_cpu_time(LOGGING_TAG_CC_LOGGING_BEGIN);
 }
 
 void Logger::close() {
     this->fitness_file.close();
+    log_wall_clock(LOGGING_TAG_WC_LOGGING_END);
+    log_cpu_time(LOGGING_TAG_CC_LOGGING_END);
+    this->tag_file.close();
 }
 
 void Logger::log_best_fitness_per_epoch(int epoch, std::vector<double> fitness) {
     this->fitness_file << epoch;
     for (auto f : fitness) this->fitness_file << "," << f;
     this->fitness_file << std::endl;
+}
+
+void Logger::log_tag_value(tag_t tag, tag_value_t value) {
+    this->tag_file.write((const char *)(&tag), sizeof(tag));
+    this->tag_file.write((const char *)(&value), sizeof(value));
+}
+
+void Logger::log_wall_clock(tag_t tag) {
+    log_tag_value(
+        tag,
+        std::chrono::duration_cast<std::chrono::microseconds>(
+            this->clock.now() - this->start
+        ).count()
+    );
+}
+
+void Logger::log_cpu_time(tag_t tag) {
+    log_tag_value(tag, std::clock());
 }
