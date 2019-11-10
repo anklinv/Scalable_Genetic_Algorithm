15b431386677c47dc5cbbf58566403cf8a9f5acb
1011han <hannes.09@hotmail.de>
2019-11-10 12:39:52 +0100

    draft of Selection and Replacement variants

--- island/island.cpp
+++ island/island.cpp
@@ -5,6 +5,336 @@
 using namespace std;
 
 
+void Island::overwriteGene(int* newGene, int* oldGene, int geneSize) {
+    
+    for(int geneIdx = 0; geneIdx < geneSize; geneIdx++) {
+        oldGene[geneIdx] = newGene[geneIdx];
+    }
+}
+
+
+int Island::computeHammingDistance(int* firstGene, int* scndGene, int geneSize) {
+    
+    int hammingDistance = 0;
+    
+    int idxFirst = 0;
+    while(firstGene[idxFirst] != 1) { // firstGene[idxFirst] == 1
+        idxFirst++;
+    }
+    
+    int idxScnd = 0;
+    while(scndGene[idxScnd] != 1) { // scndGene[idxScnd] == 1
+        idxScnd++;
+    }
+    
+    for(int geneIdx = 0; geneIdx < geneSize; geneIdx++) {
+        
+        if (firstGene[idxFirst] != scndGene[idxScnd]) {
+            hammingDistance++;
+        }
+        
+        idxFirst = (idxFirst + 1) % geneSize;
+        idxScnd = (idxScnd + 1) % geneSize;
+    }
+    
+    return hammingDistance;
+}
+
+
+int* Island::tournamentSelection(double* fitness, int numIndividuals, // TODO: change access pattern to these variables
+                                 int tournamentSize, int numIndividualsToSample) {
+    
+    int* sampledIndividuals[numIndividualsToSample];
+    
+    for(int sampleIdx = 0; sampleIdx < numIndividualsToSample; sampleIdx++) {
+        
+        int bestIdx = -1;
+        double bestFitness = numeric_limits<double>::max(); // smaller fitness value is better
+        
+        for(int t = 0; t < tournamentSize; t++) {
+            
+            // RAND_MAX is at least 32767
+            // yields slightly skewed distribution
+            int indivIdx = rand() % numIndividuals;
+            
+            if (fitness[indivIdx] < bestFitness) { // smaller fitness value is better
+                bestIdx = indivIdx;
+                bestFitness = fitness[indivIdx];
+            }
+        }
+        
+        sampledIndividuals[sampleIdx] = indivIdx;
+        
+    } // end sample
+    
+    return sampledIndividuals;
+}
+
+
+int* Island::fitnessProportionateSelection(double* fitness, int numIndividuals, // TODO: change access pattern to these variables
+                                           int numIndividualsToSample) {
+    
+    int* sampledIndividuals[numIndividualsToSample];
+    
+    // total fitness for weighting individuals
+    double totalFitness = 0;
+    
+    for(int indivIdx = 0; indivIdx < numIndividuals; indivIdx++) {
+        totalFitness = totalFitness + fitness[indivIdx];
+    }
+    
+    for(int sampleIdx = 0; sampleIdx < numIndividualsToSample; sampleIdx++) {
+    
+        double rnd = (double)rand() / (double)RAND_MAX; // [0, 1]
+        double rndScaledToFitness = rnd * totalFitness; // [0, totalFitness]
+        
+        double tmpFitness = 0;
+        
+        
+        if (rndScaledToFitness == 0) { // edge case rndScaledToFitness == 0
+            
+            if (totalFitness == 0) {
+                sampledIndividuals[sampleIdx] = numIndividuals - 1;
+            } else {
+                sampledIndividuals[sampleIdx] = 0;
+            }
+            
+            continue; // sampleIdx = sampleIdx + 1
+        }
+        
+        if (rndScaledToFitness == totalFitness) { // edge case rndScaledToFitness == totalFitness
+            
+            sampledIndividuals[sampleIdx] = numIndividuals - 1;
+            
+            continue; // sampleIdx = sampleIdx + 1
+        }
+        
+        for(int indivIdx = 0; indivIdx < numIndividuals; indivIdx++) {
+            
+            tmpFitness = tmpFitness + fitness[indivIdx]; // f_0, f_1, ..., f_n-1
+            
+            // ]0, f_0[, [f_0, f_1[, [f_1, f_2[, ..., [f_n-2, f_n-1[
+            // 0, f_n-1 edge cases covered above
+            if (tmpFitness <= rndScaledToFitness) {
+                
+                // do nothing
+                // (rndScaledToFitness lies in a subsequent interval)
+                
+            } else {
+                // first interval hit
+                sampledIndividuals[sampleIdx] = indivIdx;
+                
+                break;
+            }
+            
+        } // end fitness accumulation
+        
+    } // end sample
+    
+    return sampledIndividuals;
+}
+
+
+int* Island::stochasticUniversalSampling(double* fitness, int numIndividuals, // TODO: change access pattern to these variables
+                                         int numIndividualsToSample) {
+    
+    int* sampledIndividuals[numIndividualsToSample];
+    
+    double totalFitness = 0;
+    
+    for(int indivIdx = 0; indivIdx < numIndividuals; indivIdx++) {
+        totalFitness = totalFitness + fitness[indivIdx];
+    }
+    
+    double delta = totalFitness / numIndividualsToSample;
+    
+    double rnd = (double)rand() / (double)RAND_MAX; // [0, 1]
+    double currScaledFitness = rnd * delta; // current selection threshold
+    
+    double tmpFitness = fitness[0]; // accumulated fitness
+    
+    
+    int indivIdx = 0;
+    
+    for(int sampleIdx = 0; sampleIdx < numIndividualsToSample; sampleIdx++) {
+        
+        if (currScaledFitness == 0) { // edge case 0 (definition)
+            
+            sampledIndividuals[sampleIdx] = 0;
+            
+            currScaledFitness = currScaledFitness + delta;
+            
+            continue; // sampleIdx = sampleIdx + 1
+        }
+        
+        if (currScaledFitness == totalFitness) { // edge case f_n-1 (definition)
+            
+            sampledIndividuals[sampleIdx] = numIndividuals - 1;
+            
+            currScaledFitness = currScaledFitness + delta;
+            
+            continue; // sampleIdx = sampleIdx + 1
+        }
+        
+        // ]0, f_0[, [f_0, f_1[, ..., [f_n-2, f_n-1[
+        // 0, f_n-1 edge cases covered above
+        while(tmpFitness <= currScaledFitness) {
+            
+            tmpFitness = tmpFitness + fitness[indivIdx];
+            indivIdx = indivIdx + 1;
+        }
+        
+        sampledIndividuals[sampleIdx] = indivIdx;
+        // sampleIdx = sampleIdx + 1
+        
+    } // end sample
+    
+    
+    return sampledIndividuals;
+}
+
+
+// TODO: needs access to the ranks
+int* Island::truncationSelection(int numIndividualsToSample) {
+    
+    int* sampledIndividuals[numIndividualsToSample];
+    
+    for(int indivIdx = 0; indivIdx < numIndividualsToSample; indivIdx++) {
+     
+        sampledIndividuals[indivIdx] = indivIdx;
+    }
+    
+    return sampledIndividuals;
+}
+
+
+int* Island::pureRandomSelection(int numIndividuals, // TODO: change access to this
+                                 int numIndividualsToSample) {
+    
+    int* sampledIndividuals[numIndividualsToSample];
+    
+    for(int sampleIdx = 0; sampleIdx < numIndividualsToSample; sampleIdx++) {
+        
+        sampledIndividuals[sampleIdx] = rand() % numIndividuals;
+    }
+    
+    return sampledIndividuals;
+}
+
+
+void Island::truncationReplacement(const TravellingSalesmanProblem* tsp, int geneSize, int islandSize, // TODO: change access to this. Use the tsp pointer.
+                                   int numImmigrants, int** immigrantGenes, double* immigrantFitnesses) {
+    
+    // TODO: change access to ranks
+    int* ranks = (this->tsp)->getRanks();
+    
+    // Use (idx, fitness) pairs to facilitate sorting
+    Individual immigrants[numImmigrants];
+    
+    for(int immigrantIdx = 0; immigrantIdx < numImmigrants; immigrantIdx++) {
+        
+        (immigrants[immigrantIdx]).idx = immigrantIdx;
+        (immigrants[immigrantIdx]).fitness = immigrantFitnesses[immigrantIdx];
+    }
+    
+    // Sort incoming data in ascending order
+    // (the data of the current island is already sorted)
+    sort(immigrants, immigrants + numImmigrants);
+    
+    
+    // Determine how many individuals of the island are going to be replaced
+    int numToReplace = 0;
+    
+    int immigrantIdx = 0;
+    int islandIndivIdx = 0;
+    
+    int offsetIsland = numIndivsIsland - numImmigrants;
+    
+    while(immigrantIdx < numImmigrants
+          && islandIndivIdx < numImmigrants) {
+        
+        double currFitnessImmigrant = (immigrants[immigrantIdx]).fitness;
+        double currFitnessIslandIndiv = (this->tsp)->getFitness(ranks[offsetIsland + islandIndivIdx]);
+        
+        if(currFitnessImmigrant < currFitnessIslandIndiv) { // immigrant is better
+            
+            numToReplace++;
+            
+            immigrantIdx++;
+            islandIndivIdx++;
+            
+        } else { // island individual is better
+            
+            islandIndivIdx++;
+        }
+    }
+    
+    // Just throw out the numToReplace weakest individuals of the island by replacing their gene
+    // - the fitness is updated too
+    // - the ranks stored in the TSP object are no longer correct after this step
+    for(int indivIdx = 0; indivIdx < numToReplace; indivIdx++) {
+        
+        int* currGene = (this->tsp)->getGene(ranks[(islandSize - numImmigrants) + indivIdx]);
+        int* newGene = immigrantGenes[immigrants[indivIdx].idx];
+        
+        overwriteGene(newGene, currGene, geneSize);
+        
+        (this->tsp)->setFitness(ranks[(islandSize - numImmigrants) + indivIdx],
+                                immigrants[indivIdx].fitness);
+    }
+    
+}
+
+
+void Island::pureRandomReplacement(int islandSize, int geneSize, // TODO: change access to these variables
+                                   int numImmigrants, int** immigrantGenes, double* immigrantFitnesses) {
+    
+    for(int immigrantIdx = 0; immigrantIdx < numImmigrants; immigrantIdx++) {
+        
+        int indivToReplace = rand() % islandSize;
+        
+        int* currGene = (this->tsp)->getGene(indivToReplace);
+        int* newGene = immigrantGenes[immigrantIdx];
+        
+        overwriteGene(newGene, currGene, geneSize);
+        
+        (this->tsp)->setFitness(indivToReplace, immigrantFitnesses[immigrantIdx]);
+    }
+    
+}
+
+
+void Island::crowdingReplacement(int geneSize, int islandSize, // TODO: change access to these variables
+                                 int crowdSize,
+                                 int numImmigrants, double* immigrantFitnesses, int** immigrantGenes) {
+    
+    for(int immigrantIdx = 0; immigrantIdx < numImmigrants; immigrantIdx++) {
+        
+        int idxClosest = -1;
+        int minHammingDist = geneSize + 1;
+        
+        for(int crowdIdx = 0; crowdIdx < crowdSize; crowdIdx++) {
+            
+            int randIndivIdx = rand() % islandSize;
+            int currHammingDist = computeHammingDistance(immigrantGenes[immigrantIdx],
+                                                         (this->tsp)->getGene(randIndivIdx));
+            
+            if (currHammingDist < minHammingDist) {
+                
+                minHammingDist = currHammingDist;
+                idxClosest = randIndivIdx;
+            }
+            
+        }
+        
+        // replace individual
+        overwriteGene(immigrantGenes[immigrantIdx], (this->tsp)->getGene(idxClosest), geneSize);
+        (this->tsp)->setFitness(idxClosest, immigrantFitnesses[immigrantIdx]);
+    }
+    
+}
+
+
 double Island::solve() {
     
     // For MPI_Allgather and to compute how much data is received
@@ -118,7 +448,7 @@ double Island::solve() {
         // - the ranks stored in the TSP object are no longer correct after this step
         for(int indivIdx = 0; indivIdx < numReplaced; indivIdx++) {
             
-            int* currGene = (this->tsp)->getGene(ranks[(numIndivsIsland - 1) - indivIdx]);
+            int* currGene = (this->tsp)->getGene(ranks[(numIndivsIsland - 1) - indivIdx]); // TODO: indexing for ranks seems to be wrong
             int* newGene = &receiveBufferGenes[incomingIndividuals[indivIdx].idx * numNodes];
             
             copyArray(newGene, currGene, numNodes);
--- island/island.hpp
+++ island/island.hpp
@@ -5,57 +5,20 @@
 
 #include <algorithm>
 #include <iostream> // for debugging
+#include <limits>
 
 #include "../sequential/travelling_salesman_problem.hpp"
 
 
 using namespace std;
 
+// set up send buffer
+// set up receive buffer
+// ---- SYNCHRONOUS exchange of data (needs send buffer & receive buffer) ----
+// integrate data
 
-/**
- Facilitates sorting individuals during a migration step
- */
-typedef struct Individual {
-    
-    int idx;
-    double fitness;
-    
-    bool operator<(const Individual& other) const {
-        return (this->fitness) < other.fitness;
-    }
-    
-} Individual;
-
-
-/**
- Helper function to swap the elements of two arrays
- */
-template<class T>
-void swapArrays(T* arrA, T* arrB, int length) {
-    
-    for(int idx = 0; idx < length; idx++) {
-        
-        T tmp = arrA[idx];
-        arrA[idx] = arrB[idx];
-        arrB[idx] = tmp;
-    }
-    
-}
-
-
-/**
- Helper function to copy the elements of an array to another one
- */
-template<class T>
-void copyArray(T* source, T* destination, int length) {
-    
-    for(int idx = 0; idx < length; idx++) {
-        
-        destination[idx] = source[idx];
-    }
-    
-}
-
+// ASYNCHRONOUS
+// ?
 
 class Island {
     
@@ -65,6 +28,9 @@ public:
      An Island wraps around a TravellingSalesmanProblem
      - Fitness evaluation (C_eval) , cross-over and mutation (C_oper) are done by the underlying TSP
      - The Island adds communication (C_comm)  and performs selection and replacement in this context (C_oper)
+     
+     Because of the migration across islands it is necessary that all ranks in MPI_COMM_WORLD call the constructor
+     with the same settings (i.e. run the same code).
      
      \param tsp a pointer to a fully initialized TSP
      \param migrationPeriod the amount of iterations between two migration steps
@@ -86,27 +52,130 @@ public:
     double solve();
     
     
+    enum MigrationTopology {
+        FULLY_CONNECTED,
+        ISOLATED,
+        RING
+    };
+
+    enum SelectionPolicy {
+        TRUNCATION, // ok
+        FITNESS_PROPORTIONATE_SELECTION, // ok
+        STOCHASTIC_UNIVERSAL_SAMPLING, // ok
+        TOURNAMENT_SELECTION, // ok
+        PURE_RANDOM // ok
+    };
+
+    enum ReplacementPolicy {
+        TRUNCATION, // ok
+        DEJONG_CROWDING, // ok
+        PURE_RANDOM // ok
+    };
+    
+    
 private:
     
-    /**
-     Use a pointer to avoid dealing with object initialization
-     */
-    TravellingSalesmanProblem* tsp;
+    /// Helps sorting indices of individuals according to fitness
+    typedef struct Individual {
+        
+        int idx;
+        double fitness;
+        
+        bool operator<(const Individual& other) const {
+            return (this->fitness) < other.fitness;
+        }
+        
+    } Individual;
+    
+    
+    /// Use a pointer to avoid dealing with object initialization
+    const TravellingSalesmanProblem* TSP;
+ 
+    
+    /// Migration topology (connections between islands)
+    const MigrationTopology MIGRATION_TOPOLOGY;
+
+    /// Selection policy (source island)
+    const SelectionPolicy SELECTION_POLICY;
+    
+    /// Replacement policy (destination island)
+    const ReplacementPolicy REPLACEMENT_POLICY;
+    
     
     /**
      The amount of iterations of the algorithm between two migration steps
      */
-    int migrationPeriod;
+    const int MIGRATION_PERIOD;
     
     /**
      The amount of individuals each island sends to all other islands
      */
-    int migrationAmount;
+    const int MIGRATION_AMOUNT;
     
     /**
      The number of migration periods. The overall number of iterations is given by numPeriods * migrationPeriod.
      */
-    int numPeriods;
+    const int NUM_PERIODS;
+    
+    
+    /// Replaces the geneSize entries of oldGene with the geneSize entries of newGene. This corresponds to replacing
+    /// an individual (essentially a gene and a fitness value based thereon) of a population with a new one.
+    void overwriteGene(int* newGene, int* oldGene, int geneSize);
+    
+    /// Computes the Hamming distance between two genes. Each path (gene) is mapped to a list of cities starting
+    /// with the city indexed 1. The Hamming distance is computed as the amount of list indices where the list elements
+    /// are different.
+    /// approach to compute the Hamming Distance
+    /// search for city 1 and then to a cyclic comparison - 1 is safer than 0 because it should work for indexing starting at 0
+    /// as well as for indexing starting at 1
+    /// as the approach is cyclic the geneSize is needed for wrapping around
+    int computeHammingDistance(int* firstGene, int* scndGene, int geneSize);
+    
+    
+    /// Does fitness proportionate selection (roulette wheel selection). The selected individuals are drawn from a probability
+    /// distribution where the individuals are weighted with their fitness values. The fitness values do not have to be sorted. This
+    /// function does several O(n) linear passes over the fitness values where n is the population size. Returns the indices of
+    /// the numIndividualsToSample selected individuals as array.
+    int* fitnessProportionateSelection(double* fitness, int numIndividuals, // TODO: change access pattern to these variables
+                                       int numIndividualsToSample);
+        
+    /// Does stochastic universal sampling. This function does two O(n) linear passes over the fitness values where n is the
+    /// population size. Returns the indices of the numIndividualsToSample selected individuals as array.
+    int* stochasticUniversalSampling(double* fitness, int numIndividuals, // TODO: change access pattern to these variables
+                                     int numIndividualsToSample, int* sampledIndividuals);
+    
+    /// Does tournament selection. Returns the indices of numIndividualsToSample selected individuals as array. For each
+    /// individual to be selected, tournamentSize individuals are sampled uniformly at random and the best individual thereof
+    /// is selected. Standard values for tournamentSize are 2 or 3.
+    int* tournamentSelection(double* fitness, int numIndividuals, // TODO: change access pattern to these variables
+                             int tournamentSize, int numIndividualsToSample);
+    
+    /// Returns the indices of the numIndividualsToSample best individuals as an array. The fitness values are assumed to be sorted in
+    /// ascending order. Lower fitness is assumed to be better.
+    int* truncationSelection(int numIndividualsToSample); // TODO: add access to the ranks
+    
+    /// Returns the indices of numIndividualsToSample randomly chosen individuals. The individuals are sampled uniformly at random.
+    /// A specific individual can be selected multiple times.
+    int* pureRandomSelection(int numIndividuals, // TODO: change access to this
+                             int numIndividualsToSample)
+    
+    /// geneSize is numNodes of the TSP graph
+    /// islandSize is the number of individuals at the island
+    /// ranks: individuals are assumed to be sorted. 0 is best, islandSize-1 is worst
+    void truncationReplacement(const TravellingSalesmanProblem* tsp, int geneSize, int islandSize, // TODO: change access to this. Use the tsp pointer.
+                               int numImmigrants, int** immigrantGenes, double* immigrantFitnesses)
+    
+    /// All individuals to be replaced are chosen uniformly at random. It is possible that an immigrant is itself replaced
+    /// by a subsequent one.
+    void pureRandomReplacement(int islandSize, int geneSize, // TODO: change access to these variables
+                               int numImmigrants, int** immigrantGenes, double* immigrantFitnesses)
+    
+    /// Uses crowding for replacement. For each immigrant, crowdSize individuals are sampled uniformly at random
+    /// and their Hamming Distance to the immigrant is computed. The individual which has the smallest Hamming
+    /// Distance to the immigrant is replaced. It is possible that an immigrant is itself replaced by a subsequent one.
+    void crowdingReplacement(int geneSize, int islandSize, // TODO: change access to these variables
+                             int crowdSize,
+                             int numImmigrants, double* immigrantFitnesses, int** immigrantGenes)
     
 };
 
