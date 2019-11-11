4d7edd0426163301388cc60d72ee8c02725f1317
MacPherson Justin <justin.macpherson@scs.ch>
2019-11-11 17:45:09 +0100

    manually added files from cuda branch

---
+++ cuda/official.h
@@ -0,0 +1,16 @@
+#ifndef __KERNEL128_WINOGRAD_H__
+#define __KERNEL128_WINOGRAD_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char inputName[] = "data/tsp.bin";
+
+int traveling_salesman_problem();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
