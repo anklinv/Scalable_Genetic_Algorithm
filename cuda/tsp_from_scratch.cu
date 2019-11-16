6b5e0519846d90109774de91cc0673ed695c709f
sem19h1 Justin MacPherson <sem19h1@monch2.ethz.ch>
2019-11-16 19:50:17 +0100

    add file to perform fitness evaluations of a population over a number of generations

---
+++ cuda/tsp_from_scratch.cu
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <set>
+#include <chrono>
+#include <array>
+#include <cassert>
+#include <math.h>
+
+#define POP_SIZE 1000
+#define PROB_SIZE 48
+#define EPOCHS 100
+#define ISLANDS 4
+#define POP(i,j) population[i * PROB_SIZE + j]
+#define DIST(i,j) problem[i * PROB_SIZE + j]
+
+
+using namespace std;
+
+__global__ void rank_individuals(int *population, float *problem, float *fitness) {
+	//blockDim: number of threads in block
+	//gridDim: number of blocks in grid
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	for (int i = index; i < POP_SIZE; i++) {
+		for (int j = 0; j < PROB_SIZE - 1; j++) {
+			fitness[i] += DIST(j,j+1);
+		}
+		fitness[i] += DIST(PROB_SIZE - 1, 0);
+		fitness[i] = 1/fitness[i];
+	}
+}
+
+int main (void) {
+
+	//arrays needed for problem
+	int *population;
+	float *problem;
+	float *fitness;
+
+	//random device
+	random_device rd;
+	mt19937 gen = mt19937(rd());
+
+	//Unified memory (accessible from CPU and GPU)
+	cudaMallocManaged(&problem, PROB_SIZE * PROB_SIZE *sizeof(float));
+	cudaMallocManaged(&population, POP_SIZE * PROB_SIZE *sizeof(int));
+	cudaMallocManaged(&fitness, POP_SIZE *sizeof(float));
+
+	//initialize sequence of matrix indices to be used for initializing the population
+	vector<int> tmp_indices(PROB_SIZE);
+    	for (int i = 0; i < PROB_SIZE; ++i) {
+        	tmp_indices[i] = i;
+    	}
+
+	//initialize problem (random for now, from file later)
+	for (int i = 0; i < PROB_SIZE * PROB_SIZE; i++) {
+		problem[i] = rand() % 100;
+	}
+
+	//initialize population (random) and fitness (zero)
+	for (int i = 0; i < POP_SIZE; ++i) {
+        	shuffle(tmp_indices.begin(), tmp_indices.end(), gen);
+        	for (int j = 0; j < PROB_SIZE; ++j) {
+            		POP(i,j) = tmp_indices[j];
+        	}
+		fitness[i] = 0.0f;
+    	}
+
+	//run GA
+	for (int i = 0; i < EPOCHS; i++){
+		//begin epoch by ranking fitness of population
+		rank_individuals<<<ISLANDS, POP_SIZE / ISLANDS>>>(population,problem,fitness);
+
+		//wait till previous kernel call returns
+		cudaDeviceSynchronize();
+	}
+
+	cudaFree(population);
+	cudaFree(problem);
+	cudaFree(fitness);
+	
+	return 0;
+}
