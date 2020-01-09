2a2d2b0bba563aace2beed04dc7c57d666cde85a
Valentin Anklin <valentin@ankl.in>
2020-01-09 16:46:35 +0100

    Add plotting scripts for final report

--- logging/plot_helper.py
+++ logging/plot_helper.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 from process_log import Tags, Log, Epochs, generate_fitness_wc_dataframe
+from tqdm.auto import tqdm
 
 
 def create_barplot(df, ax, hue="n", nr_bars=10, rnd=-1, ylog=False, thresholds=None):
@@ -135,6 +136,186 @@ def create_speedup_plot(df, ax, threshold, hue=None):
     else:
         sns.lineplot(ax=ax, x="n", y="wall clock time", data=df_speedup, legend="full")
     ax.set(xlabel='n', ylabel='speedup')
+
+
+'''
+Given a dataframe, remove the rank column by taking the best fitness values among all ranks and the maximal wall clock time
+'''
+def remove_rank(df):
+    if "rank" in df:
+        assert "fitness" in df
+        assert "wall clock time" in df
+        to_keep = list(df.columns)
+        to_keep.remove("rank")
+        to_keep.remove("fitness")
+        to_keep.remove("wall clock time")
+        df = df.groupby(to_keep, as_index=False).agg({"fitness" : "min", "wall clock time" : "max"})
+    return df
+
+
+'''
+Prints the best parallel and sequential length scores that were ever achieved
+'''
+def print_best_length(df):
+    best_fitness = df[df['fitness'] == df['fitness'].min()]['fitness'].min()
+    time_needed = np.round(df[df['fitness'] == df['fitness'].min()]['wall clock time'].max() / 1000, 1)
+    needed_cores = df[df['fitness'] == df['fitness'].min()]['n'].min()
+    print(f"Best ever parallel length {best_fitness} needed time {time_needed} with {needed_cores} cores")
+    df_sequential = df[df['n'] == 1]
+    best_fitness = df_sequential[df_sequential['fitness'] == df_sequential['fitness'].min()]['fitness'].min()
+    time_needed = np.round(df_sequential[df_sequential['fitness'] == df_sequential['fitness'].min()]['wall clock time'].max() / 1000, 1)
+    print(f"Best ever sequential fitness {best_fitness} needed time {time_needed} with 1 core")
+
+
+'''
+Calculates the wall clock thresholds that were achieved by all runs
+'''
+def calculate_wall_clock_time_thresholds(df, num=100):
+    assert "wall clock time" in df
+    assert "fitness" in df
+    assert "epoch" in df
+    to_keep = list(df.columns)
+    to_keep.remove("wall clock time")
+    to_keep.remove("fitness")
+    to_keep.remove("epoch")
+    max_wct = df.groupby(to_keep, as_index=False).agg({"wall clock time": "max"})["wall clock time"].min()
+    min_wct = df.groupby(to_keep, as_index=False).agg({"wall clock time": "min"})["wall clock time"].max()
+    thresholds = np.round(np.linspace(min_wct, max_wct, num=num), 0).astype(int)
+    thresholds.sort()
+    return thresholds
+
+
+'''
+Given a dataframe a thresholds, extract the best length values that were achieved after the specified threshold time
+'''
+def calculate_lengths_achieved(df, thresholds):
+    assert "fitness" in df
+    assert "wall clock time" in df
+    line_df = None
+    for threshold in tqdm(thresholds):
+        to_keep = list(df.columns)
+        to_keep.remove("fitness")
+        to_keep.remove("wall clock time")
+        if "epoch" in df:
+            to_keep.remove("epoch")
+        tmp_df = df[df["wall clock time"] >= threshold].groupby(["rep", "population", "elite_size", "migration_amount", "epochs", "log_freq", "mode", "n"]).agg({"fitness": "max"})
+        tmp_df["wall clock time"] = threshold
+        tmp_df = tmp_df.reset_index()
+
+        if line_df is None:
+            line_df = tmp_df
+        else:
+            line_df = line_df.append(tmp_df, ignore_index=True)
+    return line_df
+
+
+'''
+Generate scaling lineplots for a given dataframe and problem name.
+Uses y_scaling_limits to determine the limits and legend_n to determine the legend
+'''
+def create_scaling_lineplot(line_df, problem, palette="plasma", limits=True, ylow=None, yhigh=None, manual_legend=True, legend_n=None, y_scaling_limits=None):
+    assert not limits or ((ylow is not None and yhigh is not None) or y_scaling_limits is not None)
+    assert not manual_legend or (legend_n is not None)
+
+    plot_df = line_df.copy()
+
+    # Turn wall clock time into seconds
+    plot_df["wall clock time"] = plot_df["wall clock time"] / 1000
+
+    # Fix the color map linear scaling
+    plot_df["n"] = np.log2(plot_df["n"])
+
+    for mode in line_df["mode"].unique():
+        fig, ax = plt.subplots()
+        sns.lineplot(ax=ax, y="fitness", x="wall clock time", hue="n", data=plot_df[plot_df["mode"] == mode], palette=palette)
+        ax.set_title(f"scaling of the {mode} model")
+
+        # Figure out limits
+        if limits:
+            if ylow is not None and yhigh is not None:
+                ax.set_ylim(ylow, ylow)
+            else:
+                low, high = y_scaling_limits[problem]
+                ax.set_ylim(low, high)
+            zoom_level = "zoom"
+        else:
+            # No scaling
+            zoom_level = "full"
+
+        # Figure out legend
+        if manual_legend:
+            all_n = list(line_df["n"].unique())
+            all_n.sort()
+            index = [all_n.index(n) for n in legend_n]
+            from matplotlib.lines import Line2D
+            if isinstance(palette, str):
+                custom_lines = [Line2D([0], [0], color=plt.get_cmap(palette)(c / (len(all_n) - 1)), lw=2) for c in index]
+            else:
+                custom_lines = [Line2D([0], [0], color=palette[i], lw=2) for i in index]
+            ax.legend(handles=custom_lines, title="n", loc="upper right", labels=list(map(str, legend_n)))
+
+        ax.get_yaxis().set_label_text("length")
+        ax.get_xaxis().set_label_text("wall clock time [s]")
+        fig.savefig(f"scaling_{mode}_{problem}_{zoom_level}.svg")
+
+
+'''
+Create speedup plot from dataframe and problem name
+Can specify thresholds directly, otherwise scaling_thresholds is used
+'''
+def create_scaling_speedup_plot(df, problem, xticks_n, scaling_thresholds=None, thresholds=None, baseline_df=None, static_ylim=True, y_scaling_limits_speedup=None, yticks_speedup=None):
+    assert (thresholds is not None) or (scaling_thresholds is not None)
+    assert not static_ylim or (y_scaling_limits_speedup is not None and yticks_speedup is not None)
+
+    # Figure out thresholds
+    if thresholds is None:
+        threshold_list = scaling_thresholds[problem]
+    else:
+        threshold_list = thresholds
+
+    for threshold in tqdm(threshold_list):
+        to_keep = ["rep", "mode", "n", "epoch"]
+        df_n = df.groupby(to_keep, as_index=False).agg({"fitness": "min", "wall clock time": "max"}).drop(columns=["epoch"])
+        to_keep = ["rep", "mode", "n"]
+        df_reached = df_n[df_n.fitness <= threshold].groupby(to_keep).agg({"wall clock time": "min"})
+        tmp = df_reached.reset_index()
+
+        # Figure out baseline
+        if baseline_df is not None:
+            baseline_n_rep = baseline_df[baseline_df["fitness"] <= threshold].groupby(["rep", "population"]).agg({"wall clock time" : "min"})
+            baseline = baseline_n_rep.groupby(["population"]).agg({"wall clock time" : "median"})["wall clock time"].min()
+        else :
+            baseline = tmp[tmp["n"] == 1]["wall clock time"].mean()
+
+        df_speedup = 1 / df_reached.divide(baseline, 1)
+        df_speedup = df_speedup.reset_index()
+        max_n = int(df_speedup.groupby(["mode"]).agg({"n" : "max"}).min())
+        df_speedup = df_speedup[df_speedup["n"] <= max_n]
+
+        if static_ylim:
+            ylim = y_scaling_limits_speedup[problem]
+            xlim = 64
+        else:
+            ylim = int(np.ceil(df_speedup["wall clock time"].max()))
+            xlim = int(df_speedup["n"].max())
+
+        # Plot the speedup
+        fig, ax = plt.subplots()
+        legend = ["island", "naive"]
+        sns.lineplot(ax=ax, x="n", y="wall clock time", hue="mode", hue_order=["island", "naive"], data=df_speedup,
+                     legend="full", err_style="bars", err_kws={"ecolor" : "k", "fmt" : "o"}, estimator=np.median, ci=95)
+        plt.plot([1,ylim],[1, ylim], "--", color='#888888')
+        ax.set(xlabel='n', ylabel='speedup')
+        ax.set_xlim(1, xlim)
+        ax.set_ylim(1, ylim)
+        ax.xaxis.set_major_locator(plt.FixedLocator(xticks_n))
+        ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
+        if static_ylim:
+            ax.yaxis.set_major_locator(plt.FixedLocator(yticks_speedup[problem]))
+            ax.yaxis.set_minor_locator(plt.MultipleLocator(1))
+        plt.legend(legend)
+        ax.set_title(f"speedup island model vs naive model (threshold {threshold})")
+        fig.savefig(f"speedup_{problem}_{threshold}.svg")
 
 
 if __name__ == "__main__":
