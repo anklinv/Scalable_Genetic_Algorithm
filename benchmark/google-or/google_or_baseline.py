393fc38668f44627301efa379069287fde5475e0
Valentin Anklin <valentin@ankl.in>
2020-01-10 12:28:34 +0100

    Fix baseline implementation and add benchmark

--- benchmark/google-or/google_or_baseline.py
+++ benchmark/google-or/google_or_baseline.py
@@ -2,8 +2,12 @@
 
 from __future__ import print_function
 import argparse
+import time
+from tqdm import tqdm
+import pandas as pd
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
+
 
 def create_data_model(data):
     """Stores the data for the problem."""
@@ -18,9 +22,21 @@ def create_data_model(data):
     data['depot'] = 0
     return data
 
+
 def print_solution(manager, routing, assignment):
     """Prints assignment on console."""
-    print('Path length achieved: {}'.format(assignment.ObjectiveValue()))
+    print('Objective: {} miles'.format(assignment.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = 'Route for vehicle 0:\n'
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} ->'.format(manager.IndexToNode(index))
+        previous_index = index
+        index = assignment.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    print(plan_output)
+    plan_output += 'Route distance: {}miles\n'.format(route_distance)
 
 
 def solve_tsp(data):
@@ -58,13 +74,25 @@ def solve_tsp(data):
 
     # Print solution on console.
     if assignment:
-        print_solution(manager, routing, assignment)
-        
+        return assignment.ObjectiveValue()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Running some experiments")
-    parser.add_argument('-d', type=str, default="a280", dest="data",
-                        help="Data file to run")
+    parser.add_argument('--rep', type=int, default=1, dest="rep",
+                        help="Number of repetitions to run")
+    parser.add_argument('-d', type=str, default=["a280"], nargs="+", dest="data",
+                        help="Data files to run")
     args = parser.parse_args()
-    
-    solve_tsp(args.data)
+
+    measurements = list()
+    for i in tqdm(range(args.rep)):
+        for data in args.data:
+            time1 = time.time()
+            length = solve_tsp(data)
+            time2 = time.time()
+            time_needed = (time2-time1)*1000.0
+            measurements.append([data, i, length, time_needed])
+    df = pd.DataFrame(measurements, columns=["data", "rep", "length", "time"])
+    df.to_csv("google_or_benchmark.csv", index=False)
+
