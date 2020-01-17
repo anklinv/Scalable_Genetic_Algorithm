3d370daafbb7e7cc5d8a60ead19b04102f039174
Valentin Anklin <valentin@ankl.in>
2020-01-17 22:40:17 +0100

    Cleanup

--- experiments/population_test_generator.py
+++ experiments/population_test_generator.py
@@ -1,43 +1,37 @@
 import json
 import math
 
-repetitions = 30
+repetitions = 10
 
-# Result of grid search
-best_population = {
-    "u2319": 32,
-    "d1291": 64,
-    "a280": 2048
-}
 base_n = 4
-nr_islands = [1, 2, 4, 8, 12, 16, 24, 32, 48, 64]
-population_size = lambda islands, data: max(islands, int(best_population[data] / (islands / base_n)))
+populations = [2, 3, 4, 6, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
 
-migration_period = 25
-migration_amount = lambda x: x // 8
 elite_size = lambda x: x // 2
 log_freq = lambda x: max(1, x // 1000)
 
 base_times = {
-    "u2319": 78.710,
+    "bier127": 2.793,
+    "a280": 4.491,
     "d1291": 16.946,
-    "a280": 4.491
+    "u2319": 78.710
 }
 
 # in minutes
 target_runtimes = {
+    "bier127": 3,
     "a280": 1,
     "d1291": 2,
-    "u2319": 4
+    "u2319": 3
 }
 
 base_population = 64
 base_epochs = 100000
 
 total_runtime = 0
+
 for data in base_times.keys():
     experiment = dict()
-    experiment["name"] = f"scaling test {data}"
+    experiment["name"] = f"population test {data}"
     experiment["repetitions"] = repetitions
     fixed_params = dict()
     fixed_params["--data"] = data + ".csv"
@@ -45,42 +39,35 @@ for data in base_times.keys():
     fixed_params["--migration_topology"] = "fully_connected"
     fixed_params["--selection_policy"] = "truncation"
     fixed_params["--replacement_policy"] = "truncation"
-    fixed_params["--migration_period"] = migration_period
+    fixed_params["mode"] = "sequential"
+    fixed_params["-n"] = 1
     experiment["fixed_params"] = fixed_params
     variable_params = dict()
     pop = dict()
     pop["type"] = "tuple"
-    pop["names"] = ["--population", "--elite_size", "-n", "--epochs", "--log_freq", "--migration_amount"]
+    pop["names"] = ["--population", "--elite_size", "--epochs", "--log_freq"]
     values = list()
-    job_count = 0
     estimated_runtime = target_runtimes[data]
-    for n in nr_islands:
-        population = population_size(n, data)
-        job_count += 1
-        value = dict()
+    for population in populations:
         multiplier = base_population / population
         epochs = int(round((estimated_runtime * 60 / base_times[data]) * base_epochs * multiplier, 0))
         val = [
             population,
             elite_size(population),
-            n,
             epochs,
-            log_freq(epochs),
-            migration_amount(population)
+            log_freq(epochs)
         ]
+        value = dict()
         value["value"] = val
         values.append(value)
     pop["values"] = values
     variable_params["pop"] = pop
-    variable_params["mode"] = {
-        "type": "list",
-        "list": ["island", "naive"]
-    }
     experiment["variable_params"] = variable_params
-    with open(f"scaling_test_{data}.json", mode="w") as file:
+    with open(f"population_test_{data}.json", mode="w") as file:
         json.dump(experiment, file, indent=2)
 
-    data_runtime = estimated_runtime * job_count * 2 * repetitions / 60
+    data_runtime = estimated_runtime * len(populations) * repetitions / 60
+    print(f"Estimated run time for {data}: {data_runtime} h per problem")
     total_runtime += data_runtime
-    print(f"Estimated run time for {data}: {data_runtime} h")
-print(f"Estimated total run time: {total_runtime} h")
+
+print(f"Estimated total time: {total_runtime} h per problem")
--- experiments/scaling_json_generator.py
+++ experiments/scaling_json_generator.py
@@ -1,35 +1,47 @@
 import json
 import math
 
-repetitions = 10
+repetitions = 30
 
+# Result of grid search
+best_population = {
+
+    "u2319": 32,
+    "d1291": 64,
+    "a280": 2048,
+    "bier127": 32768
+}
 base_n = 4
-populations = [2, 3, 4, 6, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
+nr_islands = [1, 2, 4, 8, 12, 16, 24, 32, 48, 64]
+population_size = lambda islands, data: max(islands, int(best_population[data] / (islands / base_n)))
 
+migration_period = 25
+migration_amount = lambda x: x // 8
 elite_size = lambda x: x // 2
 log_freq = lambda x: max(1, x // 1000)
 
 base_times = {
-    "a280": 4.491,
+    "u2319": 78.710,
     "d1291": 16.946,
-    "u2319": 78.710
+    "a280": 4.491,
+    "bier127": 2.793
 }
 
 # in minutes
 target_runtimes = {
     "a280": 1,
     "d1291": 2,
-    "u2319": 3
+    "u2319": 4,
+    "bier127" : 3
 }
 
 base_population = 64
 base_epochs = 100000
 
 total_runtime = 0
-
 for data in base_times.keys():
     experiment = dict()
-    experiment["name"] = f"population test {data}"
+    experiment["name"] = f"scaling test {data}"
     experiment["repetitions"] = repetitions
     fixed_params = dict()
     fixed_params["--data"] = data + ".csv"
@@ -37,35 +49,42 @@ for data in base_times.keys():
     fixed_params["--migration_topology"] = "fully_connected"
     fixed_params["--selection_policy"] = "truncation"
     fixed_params["--replacement_policy"] = "truncation"
-    fixed_params["mode"] = "sequential"
-    fixed_params["-n"] = 1
+    fixed_params["--migration_period"] = migration_period
     experiment["fixed_params"] = fixed_params
     variable_params = dict()
     pop = dict()
     pop["type"] = "tuple"
-    pop["names"] = ["--population", "--elite_size", "--epochs", "--log_freq"]
+    pop["names"] = ["--population", "--elite_size", "-n", "--epochs", "--log_freq", "--migration_amount"]
     values = list()
+    job_count = 0
     estimated_runtime = target_runtimes[data]
-    for population in populations:
+    for n in nr_islands:
+        population = population_size(n, data)
+        job_count += 1
+        value = dict()
         multiplier = base_population / population
         epochs = int(round((estimated_runtime * 60 / base_times[data]) * base_epochs * multiplier, 0))
         val = [
             population,
             elite_size(population),
+            n,
             epochs,
-            log_freq(epochs)
+            log_freq(epochs),
+            migration_amount(population)
         ]
-        value = dict()
         value["value"] = val
         values.append(value)
     pop["values"] = values
     variable_params["pop"] = pop
+    variable_params["mode"] = {
+        "type": "list",
+        "list": ["island", "naive"]
+    }
     experiment["variable_params"] = variable_params
-    with open(f"population_test_{data}.json", mode="w") as file:
+    with open(f"scaling_test_{data}.json", mode="w") as file:
         json.dump(experiment, file, indent=2)
 
-    data_runtime = estimated_runtime * len(populations) * repetitions / 60
-    print(f"Estimated run time for {data}: {data_runtime} h per problem")
+    data_runtime = estimated_runtime * job_count * 2 * repetitions / 60
     total_runtime += data_runtime
-
-print(f"Estimated total time: {total_runtime} h per problem")
+    print(f"Estimated run time for {data}: {data_runtime} h")
+print(f"Estimated total run time: {total_runtime} h")
